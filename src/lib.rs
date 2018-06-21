9d5e4f56f0039517f011bc5255eb012df76641c5
Andy Bell <andy.bell.github@gmail.com>
2018-06-21 11:10:37 +0100

    Copied existing CORS middleware from pdm-server
    project. Removed path exclusions (were only intended
    to be temporary until we could identify cause of issue.
    Added in extern crate definitions (in pdm server all
    were defined in lib.rs not in the cors_middleware.rs
    file) and specified known working versions of libaries
    in the cargo.toml.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,65 @@
+#[macro_use]
+extern crate gotham_derive;
+
+extern crate futures;
+extern crate gotham;
+extern crate hyper;
+extern crate unicase;
+
+use futures::Future;
+use gotham::handler::HandlerFuture;
+use gotham::middleware::Middleware;
+use gotham::state::{FromState, State};
+use hyper::header::{
+    AccessControlAllowCredentials, AccessControlAllowHeaders, AccessControlAllowMethods,
+    AccessControlAllowOrigin, AccessControlMaxAge, Headers, Origin,
+};
+use hyper::Method;
+use unicase::Ascii;
+
+#[derive(Clone, NewMiddleware)]
+pub struct CORSMiddleware;
+
+impl Middleware for CORSMiddleware {
+    fn call<Chain>(self, state: State, chain: Chain) -> Box<HandlerFuture>
+    where
+        Chain: FnOnce(State) -> Box<HandlerFuture>,
+    {
+        let f = chain(state).map(|(state, response)| {
+            let origin = Headers::borrow_from(&state)
+                .get::<Origin>()
+                .unwrap()
+                .to_string();
+
+            let mut headers = Headers::new();
+
+            headers.set(AccessControlAllowCredentials);
+            headers.set(AccessControlAllowHeaders(vec![
+                Ascii::new("Authorization".to_string()),
+                Ascii::new("Content-Type".to_string()),
+            ]));
+            headers.set(AccessControlAllowOrigin::Value(origin));
+            headers.set(AccessControlAllowMethods(vec![
+                Method::Delete,
+                Method::Get,
+                Method::Head,
+                Method::Options,
+                Method::Patch,
+                Method::Post,
+                Method::Put,
+            ]));
+            headers.set(AccessControlMaxAge(86400));
+
+            println!("setting headers");
+            let res = response.with_headers(headers);
+
+            (state, res)
+        });
+
+        Box::new(f)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
